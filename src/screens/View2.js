4ff517912f2bd4a7ba930a3ec26aaf0b2999fa22
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-01-02 22:31:34 +0100

    Create login and register components

--- src/screens/View2.js
+++ src/screens/View2.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './View2.module.css'
+import { register } from '../services/userService';
 
 
 function View2() {
   return (
    <div className={styles.mainDiv2}>
       <h1>WIDOK 2</h1>
-       </div>
+      <button onClick={() => {
+        register("email2@email.com", "password", "name", "bio", "joined").then(() => {
+          console.log("registered in")
+        }).catch((err) => {
+          console.log(err)
+        })
+      }}>Zarejestruj</button>
+   </div>
   );
 }
 
