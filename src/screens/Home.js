6b6f5f6f8b916cc95f9d9e0b2bbfecf25286461f
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-01-02 22:30:14 +0100

    Create home screen component

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+   <div>
+       <h1>HOME</h1>
+       <h2>Congrtats you logged succesfully</h2>
+   </div>
+  );
+}
+
+export default Home;
