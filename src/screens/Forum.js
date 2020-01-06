e59f1db9fd6f0f6c61cef39ffb19bfea487727ea
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-06 20:36:11 +0100

    Created fake quetion data and map it inside forum view

--- src/screens/Forum.js
+++ src/screens/Forum.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './Forum.module.css'
 import Question from '../components/Question'
+import { database } from 'firebase';
 
 
 function Forum() {
+
+  const RandomQuestions = [{question: "PYTANIE PIERWSZE"},{question: "PYTANIE DRUGIE"}, {question: "PYTANIE TRZECIE"}]
+
+ 
   return (
     <div className={styles.mainDiv3}>
       <div>FILTRY</div>
-      <Question question="SIEMKA"/>
-      <Question question="LOOOL"/>
-      <Question question="TRZECI!"/>
-      <Question />
+    {  RandomQuestions.map((data, index)=>  <Question question={data.question} key={index}/>)}
+    
        </div>
   );
 }
