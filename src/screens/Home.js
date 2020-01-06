3ad319be35148124733cc8b66b913612394a2317
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-01-06 21:41:26 +0100

    creat simple carusel in hero section

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,31 @@
 import React from 'react';
-
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import styles from "./Home.module.css"
+ 
 
 function Home() {
+  console.log(styles)
   return (
-   <div>
-       <h1>HOME</h1>
-       <h2>Hero section here</h2>
-   </div>
+    <CarouselProvider
+        naturalSlideWidth={50}
+        naturalSlideHeight={50}
+        totalSlides={3}
+      >
+        <Slider className={styles.slider}>
+          <Slide className={styles.slide} index={0}>
+            <img src="https://www.sciencealert.com/images/2019-12/processed/CatsHaveFacialExpressionsButHardToRead_1024.jpg" alt=""/>
+            <span className={styles.heroText}>I am the first Slide.</span>
+          </Slide>
+          <Slide className={styles.slide} index={1}>I am the second Slide.</Slide>
+          <Slide className={styles.slide} index={2}>I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack className={styles.prevButton}>Back</ButtonBack>
+        <ButtonNext className={styles.nextButton}>Next</ButtonNext>
+        <DotGroup className={styles.dots}/>
+      </CarouselProvider>
   );
 }
+
 
 export default Home;
