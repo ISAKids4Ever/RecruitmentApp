13f8c06234152575247c726292cd855cb40347f6
Kinga <zawarczynska.ko@gmail.com>
2020-01-09 20:57:58 +0100

    deleted fontawesome package from package.json

--- src/components/TestIntro.js
+++ src/components/TestIntro.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styles from './TestIntro.module.css'
+import styles from './TestIntro.module.css';
 
 
 function TestIntro(props) {
@@ -8,11 +8,11 @@ function TestIntro(props) {
         <div className={styles.intro}>
           <p> Test składa się z 10 losowych pytań. Wybierz język programowania/technologię!</p>
           <div className={styles.languages}>
-              <div className={styles.language}>JavaScript</div>
+              <div className={styles.language}><i class="fab fa-js-square"></i></div>
               <div className={styles.language}>HTML</div>
               <div className={styles.language}>CSS</div>
               <div className={styles.language}>React</div>
-             
+              
           </div>
         </div>
     );
