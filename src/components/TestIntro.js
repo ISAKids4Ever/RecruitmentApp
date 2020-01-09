c2fb406e4e353d7d094dd0b33fc60406c9c55eda
Kinga <zawarczynska.ko@gmail.com>
2020-01-09 19:37:21 +0100

    added testIntro component

---
+++ src/components/TestIntro.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import styles from './TestIntro.module.css'
+
+
+function TestIntro(props) {
+    const { state } = props
+    return (
+        <div className={styles.intro}>
+          <p> Test składa się z 10 losowych pytań. Wybierz język programowania/technologię!</p>
+          <div className={styles.languages}>
+              <div className={styles.language}>JavaScript</div>
+              <div className={styles.language}>HTML</div>
+              <div className={styles.language}>CSS</div>
+              <div className={styles.language}>React</div>
+             
+          </div>
+        </div>
+    );
+}
+
+export default TestIntro;
--- src/screens/Tests.js
+++ src/screens/Tests.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Tests.module.css";
 import TestQuestion from "../components/TestQuestion";
+import TestIntro from "../components/TestIntro";
 
 
 
 function View1() {
   return (
    <div className={styles.mainDiv1}>
-       <TestQuestion/>
+       <TestQuestion question='pierwsze pytanie' answear1='pierwsza odp' answear2='pierwsza odp' answear3='pierwsza odp'/>
+       <TestIntro/>
    </div>
   );
 }
