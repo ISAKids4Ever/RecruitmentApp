22a28ed1b3bcec6c0f5a53e098ff6b3d7a59cd02
Kinga <zawarczynska.ko@gmail.com>
2020-01-10 22:24:19 +0100

    added some basic question and mapped them in a view, now trying to solve draw

--- src/screens/Tests.js
+++ src/screens/Tests.js
@@ -6,11 +6,18 @@ import TestIntro from "../components/TestIntro";
 
 
 function View1() {
+
+  const basicQuestions =[{question: "pytanko 1", answear1: "odp 1", answear2: "odp 2", answear3: "odp 3"},
+                         {question: "pytanko 2", answear1: "odp 1.2", answear2: "odp 2.2", answear3: "odp 3.2"},
+                         {question: "pytanko 3", answear1: "odp 1.3", answear2: "odp 2.3", answear3: "odp 3.3"}
+]
   return (
    <div className={styles.mainDiv1}>
-       <TestQuestion question='pierwsze pytanie' answear1='pierwsza odp' answear2='pierwsza odp' answear3='pierwsza odp'/>
        <TestIntro/>
-   </div>
+      { basicQuestions.map((data, index)=>  <TestQuestion question={data.question} answear1={data.answear1} answear2={data.answear2} answear3={data.answear3} key={index}/>)}
+       </div>
+  
+   
   );
 }
 
