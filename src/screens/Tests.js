efc74f21298fdf2ec11e55cc11bd160a0a220d20
Kinga <zawarczynska.ko@gmail.com>
2020-01-12 18:36:42 +0100

    changed names of variables

--- src/screens/Tests.js
+++ src/screens/Tests.js
@@ -5,19 +5,18 @@ import TestIntro from "../components/TestIntro";
 
 
 function View1() {
-
   const basicQuestions =[{question: "pytanko 1", answear1: "odp 1", answear2: "odp 2", answear3: "odp 3"},
                          {question: "pytanko 2", answear1: "odp 1.2", answear2: "odp 2.2", answear3: "odp 3.2"},
                          {question: "pytanko 3", answear1: "odp 1.3", answear2: "odp 2.3", answear3: "odp 3.3"}
 ]
-const [questionsToShuffle, setQuestionsToShuffle] = useState(basicQuestions)
+const [questionsDisplay, setQuestionsDisplay] = useState(basicQuestions)
 
   function shuffle(a) {
     const newQuestions = [...a]
     for (let i = newQuestions.length -1; i>0; i--){
     const j = Math.floor(Math.random()*(i+1));
     [newQuestions[i], newQuestions[j]] = [newQuestions[j], newQuestions[i]];}
-    setQuestionsToShuffle(newQuestions)
+    setQuestionsDisplay(newQuestions)
     console.log(newQuestions)
  }
 
@@ -29,8 +28,7 @@ const [questionsToShuffle, setQuestionsToShuffle] = useState(basicQuestions)
   return (
    <div className={styles.mainDiv1}>
        <TestIntro/>
-       <button onClick={() => console.log(basicQuestions)}>HALO</button>
-      {questionsToShuffle.map((data, index)=>  <TestQuestion question={data.question} answear1={data.answear1} answear2={data.answear2} answear3={data.answear3} key={index}/>)}
+      {questionsDisplay.map((data, index)=>  <TestQuestion question={data.question} answear1={data.answear1} answear2={data.answear2} answear3={data.answear3} key={index}/>)}
        </div>
   
    
