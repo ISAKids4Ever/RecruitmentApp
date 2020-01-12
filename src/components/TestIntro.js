5415b76385c0d26a7df313e07e34a14c7074d417
Kinga <zawarczynska.ko@gmail.com>
2020-01-12 19:24:04 +0100

    standardized quotation marsk in TestIntro component

--- src/components/TestIntro.js
+++ src/components/TestIntro.js
@@ -4,14 +4,14 @@ import styles from './TestIntro.module.css';
 
 function TestIntro() {
     const [hidden, setHidden] = useState('visible')
-    if (hidden === "none"){
+    if (hidden === 'none'){
         return null
     } else {
     return (
         <div className={styles.intro}>
           <p> Test składa się z 10 losowych pytań. Wybierz język programowania/technologię!</p>
           <div className={styles.languages}>
-              <div className={styles.language}><i className="fab fa-js-square"></i></div>
+              <div className={styles.language}><i className='fab fa-js-square'></i></div>
               <div className={styles.language}>HTML</div>
               <div className={styles.language}>CSS</div>
               <div className={styles.language}>React</div>    
