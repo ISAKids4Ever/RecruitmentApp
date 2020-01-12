b869939d4c66ce22c878e3d95af8fbab1a4e514e
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-12 15:15:33 +0100

    Added react simple animate to Forum Question List

--- src/components/Forum/ItemsList.js
+++ src/components/Forum/ItemsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Item from './Item'
+import Item from './Item';
+import { Animate, AnimateGroup } from 'react-simple-animate'
 import styles from './ItemsList.module.css'
 
 
@@ -7,11 +8,20 @@ import styles from './ItemsList.module.css'
 
 
 const ItemsList = (props) => {
-   const { Questions } = props
-    return(
+    const { Questions } = props
+    return (
         <div>
-              <div>FILTRY</div>
-      {Questions.map((data, index) => <Item question={data.question} key={index} />)}
+            <div>FILTRY</div>
+           
+                {Questions.map((data, index) => <AnimateGroup play >
+                    <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} duration={1.5} sequenceIndex={index}>
+                        <Item question={data.question} key={index} />
+                        </Animate>
+                        </AnimateGroup >)}
+
+             
+
+
         </div>
     )
 }
