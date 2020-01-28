bb30fa72f1ea0a221684f461ba63397e9d63dd65
Kinga <zawarczynska.ko@gmail.com>
2020-01-28 21:34:07 +0100

    some last session changes

--- src/components/TestIntro.js
+++ src/components/TestIntro.js
@@ -12,10 +12,10 @@ function TestIntro() {
         <div className={styles.intro}>
           <p> Test składa się z 10 losowych pytań. Wybierz język programowania/technologię!</p>
           <div className={styles.languages}>
-              <Button className={'iconButton'}><i className='fab fa-js-square'></i></Button>
-              <Button className={'iconButton'}><i className='fab fa-html5'></i></Button>
-              <Button className={'iconButton'}><i className='fab fa-css3-alt'></i></Button>
-              <Button className={'iconButton'}><i className='fab fa-react'></i></Button>  
+              <Button className={'iconButton iconButtonTest'}><i className='fab fa-js-square'></i></Button>
+              <Button className={'iconButton iconButtonTest'}><i className='fab fa-html5'></i></Button>
+              <Button className={'iconButton iconButtonTest'}><i className='fab fa-css3-alt'></i></Button>
+              <Button className={'iconButton iconButtonTest'}><i className='fab fa-react'></i></Button>  
           </div>
           <Button onClick={() => setHidden('none')} >Rozpocznij test!</Button>
         </div>
