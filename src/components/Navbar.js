8c3623d60750042b69de352bf83e259d144266ac
wszczawinski <w.k.szczawinski@gmail.com>
2020-02-19 21:05:26 +0100

    Minor fix - navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,34 +8,27 @@ function Navbar(props) {
   const { profile, logout, login } = props;
   return (
     <nav className={styles.navbar}>
+
       <div className={styles.navLogo}>
         <Link to="/"><img src={logo} alt='intervyou logo'></img></Link>
       </div>
       
       <div id={styles.navViewList}>
-        <ol>
-
-          <li>
-            <Link to="/testy">Testy</Link>
-          </li>
-          <li>
-            <Link to="/pytania">Fiszki</Link>
-          </li>
-          <li>
-            <Link to="/forum">Forum</Link>
-          </li>
-          {login && <li>
-            <Link to="/zaloguj">Zaloguj się</Link>
-          </li>}
-          {profile && <li>
-            <Link to="/view4">WIDOK 4</Link>
-          </li>}
-          {logout && <li>
-            <button onClick={() => firebase.auth().signOut()}>Log out</button>
-          </li>}
+        
+        <Link to="/testy">Testy</Link>
           
-        </ol>
+        <Link to="/pytania">Fiszki</Link>
+          
+        <Link to="/forum">Forum</Link>
+          
+        {login && <Link to="/zaloguj">Zaloguj się</Link>}
+        
+        {profile && <Link to="/view4">WIDOK 4</Link> }
+        
+        {logout && <button onClick={() => firebase.auth().signOut()}>Log out</button>}
+        
       </div>
+      
     </nav>
   );
 }
