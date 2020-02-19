2960eb3d6d14e451ad70d63ffce30597405fa249
wszczawinski <w.k.szczawinski@gmail.com>
2020-02-19 21:21:09 +0100

    All fontawesome icons deleted + cdn

--- src/components/TestIntro.js
+++ src/components/TestIntro.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './TestIntro.module.css';
 import Button from './Button'
+import { FaJsSquare, FaHtml5, FaCss3Alt, FaReact } from 'react-icons/fa'
 
 
 function TestIntro(props) {
@@ -9,10 +10,10 @@ function TestIntro(props) {
         <div className={styles.intro}>
             <p> Test składa się z 10 losowych pytań. Wybierz język programowania/technologię!</p>
             <div className={styles.languages}>
-                <Button className={'iconButton iconButtonTest'}><i className='fab fa-js-square'></i></Button>
-                <Button className={'iconButton iconButtonTest'}><i className='fab fa-html5'></i></Button>
-                <Button className={'iconButton iconButtonTest'}><i className='fab fa-css3-alt'></i></Button>
-                <Button className={'iconButton iconButtonTest'}><i className='fab fa-react'></i></Button>
+                <Button className={'iconButton iconButtonTest'}><i className='fa-js-square'><FaJsSquare/></i></Button>
+                <Button className={'iconButton iconButtonTest'}><i className='fa-html5'><FaHtml5 /></i></Button>
+                <Button className={'iconButton iconButtonTest'}><i className='fa-css3-alt'><FaCss3Alt /></i></Button>
+                <Button className={'iconButton iconButtonTest'}><i className='fa-react'><FaReact/></i></Button>
             </div>
             <Button onClick={() => {
                 setElementToShow('TestQuestion')
