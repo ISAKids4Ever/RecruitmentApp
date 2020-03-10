3e40ba685d6bc994dbb629e1cc0f5751380c8e7d
wszczawinski <w.k.szczawinski@gmail.com>
2020-03-10 17:28:41 +0100

    Flashcard localStorage - minor refactor

--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -10,11 +10,6 @@ export function Flashcard(props) {
     const [count, setCount] = useState(0);
     const [known, setKnown] = useState(JSON.parse(localStorage.getItem('known')) || []);
     const [unknown, setUnknown] = useState(JSON.parse(localStorage.getItem('unknown')) || []);
-
-    useEffect(() => {
-        localStorage.setItem('known', JSON.stringify(known));
-        localStorage.setItem('unknown', JSON.stringify(unknown));
-    }, [known, unknown]);
     
     useEffect(() => {
         localStorage.setItem('known', JSON.stringify(known));
@@ -29,11 +24,9 @@ export function Flashcard(props) {
 
             if(unknown.includes(questions[count].id)){
                 let newUnknown = unknown.filter(item => item != parseInt(questions[count].id));
-                setUnknown(newUnknown)
+                setUnknown(newUnknown);
             }
         }
-        console.log(known);
-        console.log(unknown);
     }
 
     const addToUserUnknown = () => {
@@ -42,39 +35,36 @@ export function Flashcard(props) {
             newUnknown.push(questions[count].id);
             setUnknown(newUnknown);
             
-
             if(known.includes(questions[count].id)){
                 let newKnown = known.filter(item => item != parseInt(questions[count].id));
-                setKnown(newKnown)
+                setKnown(newKnown);
             }
         }
-        console.log(known);
-        console.log(unknown)
     }
 
-        return (
-            <div className={styles.flashcardView}>
-                <div className={styles.flashcardPlusButtons}>
-                    { (count) ?  <Button onClick={() => setCount(count - 1)} className={'iconButton'}><i><FaChevronCircleLeft/></i></Button> : <p> </p>}
-                    <div className={styles.flashcard}>
-                        <div className={styles.flashcardInner}>
-                            <div className={styles.flashcardFront}>
-                                <i><FaQuestion /></i>
-                                <p>{questions[count].question}</p>
-                            </div>
-                            <div className={styles.flashcardBack}>
-                                <i><FaCommentDots /></i>
-                                <p>{questions[count].answer}</p>
-                            </div>
+    return (
+        <div className={styles.flashcardView}>
+            <div className={styles.flashcardPlusButtons}>
+                { (count) ?  <Button onClick={() => setCount(count - 1)} className={'iconButton'}><i><FaChevronCircleLeft/></i></Button> : <p> </p>}
+                <div className={styles.flashcard}>
+                    <div className={styles.flashcardInner}>
+                        <div className={styles.flashcardFront}>
+                            <i><FaQuestion /></i>
+                            <p>{questions[count].question}</p>
+                        </div>
+                        <div className={styles.flashcardBack}>
+                            <i><FaCommentDots /></i>
+                            <p>{questions[count].answer}</p>
                         </div>
                     </div>
-                    { (count+1 < questions.length) ? <Button onClick={() => setCount(count + 1)} className={'iconButton'}><i><FaChevronCircleRight/></i></Button> : <p> </p>}
                 </div>
-                <div className={styles.flashcardUserButtons}>
-                    <Button className={'iconButton'}><i><FaSave /></i></Button>
-                    <Button className={'iconButton'} onClick={() => addToUserKnown()} ><i><FaCheck /></i></Button>
-                    <Button className={'iconButton'} onClick={() => addToUserUnknown()} ><i><FaTimes /></i></Button>
-                </div>
+                { (count+1 < questions.length) ? <Button onClick={() => setCount(count + 1)} className={'iconButton'}><i><FaChevronCircleRight/></i></Button> : <p> </p>}
             </div>
-        )
+            <div className={styles.flashcardUserButtons}>
+                <Button className={'iconButton'}><i><FaSave /></i></Button>
+                <Button className={'iconButton'} onClick={() => addToUserKnown()} ><i><FaCheck /></i></Button>
+                <Button className={'iconButton'} onClick={() => addToUserUnknown()} ><i><FaTimes /></i></Button>
+            </div>
+        </div>
+    )
 }
