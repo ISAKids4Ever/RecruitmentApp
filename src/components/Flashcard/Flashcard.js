85dd605db668d27326fbb61d4e528c4e495d8315
wszczawinski <w.k.szczawinski@gmail.com>
2020-03-11 21:12:49 +0100

    feat(flashcards): changes in html structure (+css) for future extracting single flashcard to separate function

--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -69,24 +69,26 @@ export function Flashcard(props) {
         <div className={styles.flashcardView}>
             <div className={styles.flashcardPlusButtons}>
                 { (count) ?  <Button onClick={() => setCount(count - 1)} className={'iconButton'}><i><FaChevronCircleLeft/></i></Button> : <p> </p>}
-                <div className={styles.flashcard}>
-                    <div className={styles.flashcardInner}>
-                        <div className={styles.flashcardFront}>
-                            <i><FaQuestion /></i>
-                            <p>{questions[count].question}</p>
+                <div className="flashcardPlusUserButtons">
+                    <div className={styles.flashcard}>
+                        <div className={styles.flashcardInner}>
+                            <div className={styles.flashcardFront}>
+                                <i><FaQuestion /></i>
+                                <p>{questions[count].question}</p>
+                            </div>
+                            <div className={styles.flashcardBack}>
+                                <i><FaCommentDots /></i>
+                                <p>{questions[count].answer}</p>
+                            </div>
                         </div>
-                        <div className={styles.flashcardBack}>
-                            <i><FaCommentDots /></i>
-                            <p>{questions[count].answer}</p>
-                        </div>
+                    </div>
+                    <div className={styles.flashcardUserButtons}>
+                        <Button className={'iconButton'}><i><FaSave /></i></Button>
+                        <Button className={'iconButton'} onClick={() => addToUserKnown(questions[count].id)} ><i><FaCheck /></i></Button>
+                        <Button className={'iconButton'} onClick={() => addToUserUnknown(questions[count].id)} ><i><FaTimes /></i></Button>
                     </div>
                 </div>
                 { (count+1 < questions.length) ? <Button onClick={() => setCount(count + 1)} className={'iconButton'}><i><FaChevronCircleRight/></i></Button> : <p> </p>}
-            </div>
-            <div className={styles.flashcardUserButtons}>
-                <Button className={'iconButton'}><i><FaSave /></i></Button>
-                <Button className={'iconButton'} onClick={() => addToUserKnown()} ><i><FaCheck /></i></Button>
-                <Button className={'iconButton'} onClick={() => addToUserUnknown()} ><i><FaTimes /></i></Button>
             </div>
         </div>
     )
