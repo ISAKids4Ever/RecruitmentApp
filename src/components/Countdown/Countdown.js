c5124dd465fd5b5dcc453fd920ca7c376211719e
Kinga Zawarczynska <kingazawarczynska@Kingas-MacBook-Pro.local>
2020-03-30 16:23:07 +0200

    fix(countdown): change second to minutes and seconds

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 export default function Countdown({ seconds, setTestTimeLeft }) {
     const [timeLeft, setTimeLeft] = useState(seconds);
+    const convertHMS = (value) => {
+        const sec = parseInt(value, 10);
+        let minutes = Math.floor(sec / 60);
+        let seconds = sec - (minutes * 60);
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+        return minutes+':'+seconds;
+    }
 
     useEffect(() => {
         if (!timeLeft) return;
@@ -15,7 +23,7 @@ export default function Countdown({ seconds, setTestTimeLeft }) {
     setTestTimeLeft(timeLeft);
     return (
         <div>
-            <h1>{timeLeft}</h1>
+            <h1>{convertHMS(timeLeft)}</h1>
         </div>
     );
 
