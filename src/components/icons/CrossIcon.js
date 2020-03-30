3057e0b86d18a50069a12c45f7df3159fe518b82
wszczawinski <w.k.szczawinski@gmail.com>
2020-03-30 22:55:42 +0200

    feat(icons): updated crossIcon - no offset

--- src/components/icons/CrossIcon.js
+++ src/components/icons/CrossIcon.js
@@ -1,32 +1,30 @@
 import React from 'react';
 
-const SaveIcon = ({ className = 'flashcardIcon' }) => (
-    <div >
-        <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 237.8">
-            <line
-                x1="106.2"
-                y1="89.2"
-                x2="140.7"
-                y2="124.5"
-                fill="none"
-                stroke="#e6e6e6"
-                stroke-linecap="round"
-                stroke-miterlimit="10"
-                stroke-width="6"
-            />
-            <line
-                x1="141.5"
-                y1="89.2"
-                x2="106.2"
-                y2="124.5"
-                fill="none"
-                stroke="#e6e6e6"
-                stroke-linecap="round"
-                stroke-miterlimit="10"
-                stroke-width="6"
-            />
-        </svg>
-    </div>
+const SaveIcon = ({ className }) => (
+    <svg className={className}>
+        <line
+            x1="1.6"
+            y1="1.6"
+            x2="17.1"
+            y2="17.4"
+            fill="none"
+            stroke="#e6e6e6"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="3"
+        />
+        <line
+            x1="17.5"
+            y1="1.6"
+            x2="1.6"
+            y2="17.4"
+            fill="none"
+            stroke="#e6e6e6"
+            stroke-linecap="round"
+            stroke-miterlimit="10"
+            stroke-width="3"
+        />
+    </svg>
 );
 
 export default SaveIcon;
