2f18e346887fb5deb1f735dce4169a535ac3aebf
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-01 06:53:17 -0700

    Added created date to item

--- src/components/Forum/Item.js
+++ src/components/Forum/Item.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 import styles from './Item.module.css'
 import {db} from '../../firebase'
 
 export function Item({ question }) {
 
+  const { title, created, description, qid, votes } = question
+
   function handleVote() {
 
-    const voteReference = db.collection('forum').doc(question.qid);
+    const voteReference = db.collection('forum').doc(qid);
     voteReference.get().then(doc => {
       if(doc.exists) {
         const prevVotes = doc.data().votes;
@@ -24,21 +26,23 @@ export function Item({ question }) {
   }
 
   function handleDelete() {
-    let questionDelRef = db.collection('forum').doc(question.qid);
+    let questionDelRef = db.collection('forum').doc(qid);
     questionDelRef.delete()
   }
   return (
     <div className={styles.mainDiv}>
       <div className={styles.questionSection}>
-        <div className={styles.title}><Link to={`/forum/${question.qid}`} className="link-style">{question.title}</Link></div>
-        <div className={styles.description}>{question.description}</div>
+        <div className={styles.title}><Link to={`/forum/${qid}`} className="link-style">{title}</Link></div>
+          <div>Added: {formatDistanceToNow(created, {addSuffix:true})}</div>
+          <div>Created by unknown</div>
+        <div className={styles.description}>{description}</div>
         <div className={styles.description1}>
-          <Link to={`/forum/${question.qid}`} className={styles.link}>DISCUSS</Link>
+          <Link to={`/forum/${qid}`} className={styles.link}>DISCUSS</Link>
           <div onClick={handleDelete} className={styles.handleDelete} >DELETE</div>
         </div>
       </div>
       <div className={styles.likesSection} onClick={handleVote}>
-        <div className={styles.likeSection1}>{question.votes.length}</div>
+        <div className={styles.likeSection1}>{votes.length}</div>
         <div className={styles.likeSection2} >
           <span>&#8593;</span>
         </div>
