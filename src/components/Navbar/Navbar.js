c0016cda2691523a5769dedccc69ed35495dd0b8
wszczawinski <w.k.szczawinski@gmail.com>
2020-04-10 23:41:33 +0200

    styling(navbar): new logo + login icons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,39 +2,38 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import firebase from '../../firebase';
-import logo from '../../images/logoSmallBlack.png';
+import LOGO from '../Icons/LOGO.svg';
+import SignInIcon from '../Icons/SignInIcon';
+import LogOutIcon from '../Icons/LogOutIcon';
 
 export function Navbar({ profile = false, logout = false, login = false }) {
-	return (
-		<nav className={styles.navbar}>
-			<div className={styles.navLogo}>
-				<Link to="/">
-					<img src={logo} alt="intervyou logo" />
-				</Link>
-			</div>
+    return (
+        <nav className={styles.navbar}>
+            <Link to="/" className={styles.navLogo}>
+                <img src={LOGO} alt="intervyou logo" />
+            </Link>
 
-			<div className={styles.navViewList}>
-				<NavLink to="/testy" activeClassName={styles.navActive}>
-					Testy
-				</NavLink>
-				<NavLink to="/pytania" activeClassName={styles.navActive}>
-					Fiszki
-				</NavLink>
-				<NavLink to="/forum" activeClassName={styles.navActive}>
-					Forum
-				</NavLink>
-				{login && (
-					<NavLink to="/zaloguj" activeClassName={styles.navActive}>
-						Zaloguj się
-					</NavLink>
-				)}
-				{profile && (
-					<NavLink to="/view4" activeClassName={styles.navActive}>
-						WIDOK 4
-					</NavLink>
-				)}
-				{logout && <button onClick={() => firebase.auth().signOut()}>Log out</button>}
-			</div>
-		</nav>
-	);
+            <div className={styles.navViewList}>
+                <NavLink to="/testy" activeClassName={styles.navActive}>
+                    Tests
+                </NavLink>
+                <NavLink to="/pytania" activeClassName={styles.navActive}>
+                    Flashcards
+                </NavLink>
+                <NavLink to="/forum" activeClassName={styles.navActive}>
+                    Forum
+                </NavLink>
+                {login && (
+                    <NavLink to="/zaloguj" activeClassName={styles.navActive}>
+                        <SignInIcon className={styles.loginIcon} />
+                    </NavLink>
+                )}
+                {logout && (
+                    <NavLink to="/" onClick={() => firebase.auth().signOut()}>
+                        <LogOutIcon className={styles.loginIcon} />
+                    </NavLink>
+                )}
+            </div>
+        </nav>
+    );
 }
