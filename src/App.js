94f906e73582443b6cb953d9ad24270a4721bc86
wszczawinski <w.k.szczawinski@gmail.com>
2020-04-11 18:48:46 +0200

    feat(routes): create and use  routes constants + clean up app.js

--- src/App.js
+++ src/App.js
@@ -1,68 +1,58 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,  
-  Switch
-} from "react-router-dom";
-import  { useAuth }  from "hooks";
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { useAuth } from 'hooks';
 import './App.css';
 
 // components
 import { Navbar, ItemDetails } from 'components';
 
 // screens
-import { Forum, Home, Login, Profile, Register, Flashcards, Tests } from "screens";
+import { Forum, Home, Login, Profile, Register, Flashcards, Tests } from 'screens';
 
+//routes
+import * as ROUTES from './constants/routes';
 
 const App = () => {
-  const isLoggedIn = useAuth();
+    const isLoggedIn = useAuth();
 
-  if (isLoggedIn === null) {
-    return (      
-      <div>Pobieranie danych...</div>  
-    );
-  }
+    if (isLoggedIn === null) {
+        return <div>Pobieranie danych...</div>;
+    }
 
-  if (!isLoggedIn) {
     return (
-      <Router>
-        <div>
-          <div>
-            <Navbar login/>
-          </div>
-          <Switch>
-            <Route exact path="/zaloguj" component={Login} />
-            <Route exact path="/zarejestruj" component={Register} />
-            <Route strict exact path="/testy" component={Tests}/>
-            <Route strict exact path="/pytania" component={Flashcards}/>
-            <Route strict exact path="/forum" component={Forum}/>
-            <Route path="/forum/:qid" component={ItemDetails} />
-            <Route strict exact path="/profile" component={Profile}/>
-            <Route exact path="/" component={Home} />
-            <Redirect to="/zaloguj" />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+        <Router>
+            <div>
+                {!isLoggedIn ? <Navbar login /> : <Navbar profile logout />}
+                <Switch>
+                    <Route exact path={ROUTES.HOME} component={Home} />
+                    <Route strict exact path={ROUTES.TESTS} component={Tests} />
+                    <Route strict exact path={ROUTES.FLASHCARDS} component={Flashcards} />
+                    <Route strict exact path={ROUTES.FORUM} component={Forum} />
+                    <Route path={ROUTES.ITEMDETAILS} component={ItemDetails} />
 
-  return (
-    <Router>
-      <Navbar logout/>      
-        <Switch>
-          <Redirect path="/zaloguj" to="/" />
-          <Redirect path="/zarejestruj" to="/" /> 
-          <Route strict exact path="/testy" component={Tests}/>
-          <Route strict exact path="/pytania" component={Flashcards}/>
-          <Route strict exact path="/forum" component={Forum}/>
-          <Route strict exact path="/profile" component={Profile}/>
-          <Route path="/forum/:qid" component={ItemDetails} />
-          <Route exact path="/" component={Home} />
-          <Route component={() => <h1>Nie ma takiej strony</h1>} />
-        </Switch>  
-    </Router>
-  );  
+                    {!isLoggedIn ? (
+                        <>
+                            <Route exact path={ROUTES.LOGIN} component={Login} />
+                            <Route exact path={ROUTES.REGISTER} component={Register} />
+                            <Redirect to={ROUTES.LOGIN} />
+                        </>
+                    ) : (
+                        <>
+                            <Redirect path={ROUTES.LOGIN} to={ROUTES.HOME} />
+                            <Redirect path={ROUTES.REGISTER} to={ROUTES.HOME} />
+                            <Route
+                                strict
+                                exact
+                                path={ROUTES.PROFILE}
+                                component={Profile}
+                            />
+                            <Route component={() => <h1>Nie ma takiej strony</h1>} />
+                        </>
+                    )}
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,30 +6,40 @@ import LOGO from '../Icons/LOGO.svg';
 import SignInIcon from '../Icons/SignInIcon';
 import LogOutIcon from '../Icons/LogOutIcon';
 
+import * as ROUTES from '../../constants/routes';
+
 export function Navbar({ profile = false, logout = false, login = false }) {
     return (
         <nav className={styles.navbar}>
-            <Link to="/" className={styles.navLogo}>
+            <Link to={ROUTES.HOME} className={styles.navLogo}>
                 <img src={LOGO} alt="intervyou logo" />
             </Link>
 
             <div className={styles.navViewList}>
-                <NavLink to="/testy" activeClassName={styles.navActive}>
+                <NavLink to={ROUTES.TESTS} activeClassName={styles.navActive}>
                     Tests
                 </NavLink>
-                <NavLink to="/pytania" activeClassName={styles.navActive}>
+                <NavLink to={ROUTES.FLASHCARDS} activeClassName={styles.navActive}>
                     Flashcards
                 </NavLink>
-                <NavLink to="/forum" activeClassName={styles.navActive}>
+                <NavLink to={ROUTES.FORUM} activeClassName={styles.navActive}>
                     Forum
                 </NavLink>
                 {login && (
-                    <NavLink to="/zaloguj" activeClassName={styles.navActive}>
+                    <NavLink to={ROUTES.LOGIN} activeClassName={styles.navActive}>
+                        <SignInIcon className={styles.loginIcon} />
+                    </NavLink>
+                )}
+                {profile && (
+                    <NavLink
+                        to={ROUTES.PROFILE}
+                        onClick={() => firebase.auth().signOut()}
+                    >
                         <SignInIcon className={styles.loginIcon} />
                     </NavLink>
                 )}
                 {logout && (
-                    <NavLink to="/" onClick={() => firebase.auth().signOut()}>
+                    <NavLink to={ROUTES.HOME} onClick={() => firebase.auth().signOut()}>
                         <LogOutIcon className={styles.loginIcon} />
                     </NavLink>
                 )}
