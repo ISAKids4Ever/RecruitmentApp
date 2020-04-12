1a01777e7c3f36812e4bd169bb047ac7727d631a
wszczawinski <w.k.szczawinski@gmail.com>
2020-04-12 13:18:26 +0200

    styling(flashCardIntro): based on UI

--- src/components/FlashcardsIntro/FlashcardsIntro.js
+++ src/components/FlashcardsIntro/FlashcardsIntro.js
@@ -1,21 +1,34 @@
-import React from 'react'
-import styles from './FlashcardsIntro.module.css'
+import React from 'react';
+import styles from './FlashcardsIntro.module.css';
 import { Button } from 'components';
+import ReactIcon from '../Icons/ReactIcon';
+import HTMLIcon from '../Icons/HTMLIcon';
+import CSSIcon from '../Icons/CSSIcon';
+import JSIcon from '../Icons/JSIcon';
 
+import StartButton from '../Icons/StartButton';
 
 export function FlashcardsIntro(props) {
-    const {showIntro} = props
+    const { showIntro } = props;
 
     const hideIntro = (hide) => {
-        showIntro(hide)
-    }
+        showIntro(hide);
+    };
 
     return (
         <div className={styles.FlashcardsIntro}>
             <div className={styles.FlashcardsIntroCard}>
                 <p>Poznaj typowe pytania rekrutacujne i odpowiedzi.</p>
+                <section className={styles.flashcardsTechIcons}>
+                    <ReactIcon className={styles.flashcardTechIcon} />
+                    <HTMLIcon className={styles.flashcardTechIcon} />
+                    <CSSIcon className={styles.flashcardTechIcon} />
+                    <JSIcon className={styles.flashcardTechIcon} />
+                </section>
+                <Button className={'startButton'} onClick={() => hideIntro(false)}>
+                    <StartButton />
+                </Button>
             </div>
-            <Button onClick={() => hideIntro(false)}>Wylosuj pytanie</Button>
         </div>
-    )
+    );
 }
