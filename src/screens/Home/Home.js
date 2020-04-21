71f98b510c1c4652293276393a501c605c8cf7e1
wszczawinski <w.k.szczawinski@gmail.com>
2020-04-21 21:31:25 +0200

    fix(home): backgoround styling and slides short descriptions in eng

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -6,42 +6,42 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 export function Home() {
-	const settings = {
-		dots: true,
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 8000,
-		pauseOnHover: true
-	};
-	return (
-		<div className="homepage__slider">
-			<Slider {...settings}>
-				<div className={`${styles.slideWrapper}`}>
-					<div className={`${styles.slide} ${styles.first}`}>
-						<h2 className={styles.slideHeader}>IntervYou</h2>
-						<p className={styles.slideText}>
-							Jest to aplikacja dzięki której sprawdzisz swoją wiedzę z frontendu
-						</p>
-					</div>
-				</div>
-				<div className={`${styles.slideWrapper}`}>
-					<div className={`${styles.slide} ${styles.second}`}>2</div>
-				</div>
-				<div className={`${styles.slideWrapper}`}>
-					<div className={`${styles.slide} ${styles.third}`}>3</div>
-				</div>
-				<div className={`${styles.slideWrapper}`}>
-					<div className={`${styles.slide} ${styles.fourth}`}>4</div>
-				</div>
-				<div className={`${styles.slideWrapper}`}>
-					<div className={`${styles.slide} ${styles.fifth}`}>5</div>
-				</div>
-				<div className={`${styles.slideWrapper}`}>
-					<div className={`${styles.slide} ${styles.sixth}`}>6</div>
-				</div>
-			</Slider>
-		</div>
-	);
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        pauseOnHover: true,
+    };
+    return (
+        <div className="homepage__slider">
+            <Slider {...settings}>
+                <div className={`${styles.slideWrapper}`}>
+                    <div className={`${styles.slide} ${styles.first}`}>
+                        <h2 className={styles.slideHeader}>
+                            Hello, welcome to IntervYou!
+                        </h2>
+                        <p className={styles.slideText}>
+							<br/>
+                            Here, you will learn common interview questions. You can
+                            explore Flashcards and then check your knowledge with Tests.
+                            If there is anything more you would like to ask about you can
+                            do it on our Forum.
+                        </p>
+                    </div>
+                </div>
+                <div className={`${styles.slideWrapper}`}>
+                    <div className={styles.slide}>Tests description</div>
+                </div>
+                <div className={`${styles.slideWrapper}`}>
+                    <div className={styles.slide}>Flashcards description</div>
+                </div>
+                <div className={`${styles.slideWrapper}`}>
+                    <div className={styles.slide}>Forum description</div>
+                </div>
+            </Slider>
+        </div>
+    );
 }
