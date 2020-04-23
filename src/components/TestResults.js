b0f4a3a68c046addb369d1f2ad2866256c1b4aad
Kinga <zawarczynska.ko@gmail.com>
2020-04-23 19:06:03 +0200

    feat(countdown): refresh counter after test

--- src/components/TestResults.js
+++ src/components/TestResults.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import { Button } from './';
 
-export function TestResults({ setElementToShow, userPoints, setCurrentPage, timeHasGone }) {
+export function TestResults({ setElementToShow, userPoints, setCurrentPage, timeHasGone, elementToShow }) {
     return (
         <div>
             <section>{userPoints.reduce(function (prevPoints, currentPoint) {
                 return prevPoints + currentPoint
             }, 0)}
             </section>
-            {timeHasGone && <section>You're time is up!</section>}
+            {timeHasGone && <section>Your time is up!</section>}
             <Button children='Wróć do testów' onClick={() => {
                 setElementToShow('TestIntro')
                 setCurrentPage(1)
