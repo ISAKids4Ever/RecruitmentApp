ec92bcc2986a4466bc12cc7c212b9a039c3ff5f1
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-04-24 22:33:44 +0200

    feat(flashcards): get data from firestore

--- src/screens/Flashcards/Flashcards.js
+++ src/screens/Flashcards/Flashcards.js
@@ -1,72 +1,89 @@
-import React, { useEffect, useState } from "react";
-import styles from "./Flashcards.module.css";
+import React, { useEffect, useState } from 'react';
+import styles from './Flashcards.module.css';
 
-import { FlashcardsIntro, Flashcard} from 'components';
-import { Questions } from './QuestionsBase.js';
+import { db } from '../../firebase';
+import { FlashcardsIntro, Flashcard } from 'components';
 
 export function Flashcards() {
-  const [shuffledQuestions, setShuffledQuestions] = useState(Questions);
-  const [showIntro, setShowIntro] = useState(true);
-  const [known, setKnown] = useState(JSON.parse(localStorage.getItem('known')) || []);
-  const [unknown, setUnknown] = useState(JSON.parse(localStorage.getItem('unknown')) || []);
-  const [all, setAll] = useState();
+    const [questions, setQuestions] = useState([]);
+    const [shuffledQuestions, setShuffledQuestions] = useState([]);
+    const [showIntro, setShowIntro] = useState(true);
+    const [known, setKnown] = useState(JSON.parse(localStorage.getItem('known')) || []);
+    const [unknown, setUnknown] = useState(
+        JSON.parse(localStorage.getItem('unknown')) || []
+    );
+    const [all, setAll] = useState();
 
+    useEffect(() => {
+        db.collection('flashcards').onSnapshot(handleSnapshot);
+    }, []);
 
-  useEffect(() => {
-    shuffle(Questions);
-    checkAllWithUserBase(shuffledQuestions);
-  }, []);
+    function handleSnapshot(snapshot) {
+        const questions = snapshot.docs.map((doc) => {
+            return { id: doc.id, ...doc.data() };
+        });
 
-  useEffect(() => {
-    localStorage.setItem('known', JSON.stringify(known));
-    localStorage.setItem('unknown', JSON.stringify(unknown));
-  });
-
-  const whatsDisplayed = (dataFromIntro) => {
-    setShowIntro(dataFromIntro);
-  }
-
-  const checkAllWithUserBase = (questions) =>{
-    let check = questions.map(question => question.id);
-      for (let i = 0; i < known.length; i++) {
-          check = check.filter(id => id !== known[i]);
-      }
-      for (let i = 0; i < unknown.length; i++) {
-          check = check.filter(id => id !== unknown[i]);
-      }
-    setAll(check);
-  }
-
-  const shuffle = (questions) => {
-    let shuffledQuestions = [...questions]
-    for (let i = shuffledQuestions.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [shuffledQuestions[i], shuffledQuestions[j]] = [shuffledQuestions[j], shuffledQuestions[i]];
+        setQuestions(questions);
+        shuffle(questions);
     }
-    setShuffledQuestions(shuffledQuestions);
-  }
 
-  if (showIntro) {
-    return (
-      <div className={styles.mainFlashcards}>
-        <FlashcardsIntro showIntro={whatsDisplayed}/>
-      </div>
-    )
-  }else{
-    return (
-      <div className={styles.mainFlashcards}>
-        <Flashcard 
-          questions={shuffledQuestions} 
-          all={all}
-          setAll={setAll}
-          known={known}
-          setKnown={setKnown}
-          unknown={unknown}
-          setUnknown={setUnknown}
-          checkAllWithUserBase={checkAllWithUserBase}
-          setShowIntro={setShowIntro}
-        />
-      </div>
-    )
-  }
+    useEffect(() => {
+        checkAllWithUserBase(shuffledQuestions);
+    }, [shuffledQuestions]);
+
+    useEffect(() => {
+        localStorage.setItem('known', JSON.stringify(known));
+        localStorage.setItem('unknown', JSON.stringify(unknown));
+    });
+
+    const whatsDisplayed = (dataFromIntro) => {
+        setShowIntro(dataFromIntro);
+    };
+
+    const checkAllWithUserBase = (questions) => {
+        let check = questions.map((question) => question.id);
+        for (let i = 0; i < known.length; i++) {
+            check = check.filter((id) => id !== known[i]);
+        }
+        for (let i = 0; i < unknown.length; i++) {
+            check = check.filter((id) => id !== unknown[i]);
+        }
+        setAll(check);
+    };
+
+    const shuffle = (questions) => {
+        let shuffledQuestions = [...questions];
+        for (let i = shuffledQuestions.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [shuffledQuestions[i], shuffledQuestions[j]] = [
+                shuffledQuestions[j],
+                shuffledQuestions[i],
+            ];
+        }
+        setShuffledQuestions(shuffledQuestions);
+    };
+
+    if (showIntro) {
+        return (
+            <div className={styles.mainFlashcards}>
+                <FlashcardsIntro showIntro={whatsDisplayed} />
+            </div>
+        );
+    } else {
+        return (
+            <div className={styles.mainFlashcards}>
+                <Flashcard
+                    questions={shuffledQuestions}
+                    all={all}
+                    setAll={setAll}
+                    known={known}
+                    setKnown={setKnown}
+                    unknown={unknown}
+                    setUnknown={setUnknown}
+                    checkAllWithUserBase={checkAllWithUserBase}
+                    setShowIntro={setShowIntro}
+                />
+            </div>
+        );
+    }
 }
