fe2dd54b79b33281cf02e43d5ba2b5094d1b6977
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-05-03 23:08:42 +0200

    feat(home): add images and styling for all funcionalities in app

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,6 +4,10 @@ import Slider from 'react-slick';
 import styles from './Home.module.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import HomeImage from '../../images/HomeImage.svg';
+import TestsImage from '../../images/TestsImage.svg';
+import FlashcardsImage from '../../images/FlashcardsImage.svg';
+import ForumImage from '../../images/ForumImage.svg';
 
 export function Home() {
     const settings = {
@@ -18,28 +22,73 @@ export function Home() {
     return (
         <div className="homepage__slider">
             <Slider {...settings}>
-                <div className={`${styles.slideWrapper}`}>
-                    <div className={`${styles.slide} ${styles.first}`}>
-                        <h2 className={styles.slideHeader}>
-                            Hello, welcome to IntervYou!
-                        </h2>
+                <div className={styles.slideWrapper}>
+                    <div className={styles.slide}>
+                        <img
+                            className={styles.descriptionImage}
+                            src={HomeImage}
+                            alt="homeImage"
+                        />
+                        <h2 className={styles.slideHeader}>Welcome to IntervYou!</h2>
                         <p className={styles.slideText}>
-							<br/>
-                            Here, you will learn common interview questions. You can
-                            explore Flashcards and then check your knowledge with Tests.
-                            If there is anything more you would like to ask about you can
-                            do it on our Forum.
+                            Here, you will learn common IT interview questions. <br />
+                            You can explore <strong>Flashcards</strong> and then check
+                            your knowledge with <strong> Tests</strong>. <br />
+                            If there is anything more you would like to ask about go to
+                            our <strong>Forum</strong>.
                         </p>
                     </div>
                 </div>
-                <div className={`${styles.slideWrapper}`}>
-                    <div className={styles.slide}>Tests description</div>
+                <div className={styles.slideWrapper}>
+                    <div className={styles.slide}>
+                        <img
+                            className={styles.descriptionImage}
+                            src={TestsImage}
+                            alt="testsImage"
+                        />
+                        <h2 className={styles.slideHeader}>Tests!</h2>
+                        <p className={styles.slideText}>
+                            Some more tests description below. <br />
+                            You can explore <strong>Flashcards</strong> and then check
+                            your knowledge with <strong> Tests</strong>. <br />
+                            If there is anything more you would like to ask about go to
+                            our <strong>Forum</strong>.
+                        </p>
+                    </div>
                 </div>
-                <div className={`${styles.slideWrapper}`}>
-                    <div className={styles.slide}>Flashcards description</div>
+                <div className={styles.slideWrapper}>
+                    <div className={styles.slide}>
+                        <img
+                            className={styles.descriptionImage}
+                            src={FlashcardsImage}
+                            alt="flashcardsImage"
+                        />
+                        <h2 className={styles.slideHeader}>Flashcards!</h2>
+                        <p className={styles.slideText}>
+                            Flashcards descriptions. <br />
+                            You can explore <strong>Flashcards</strong> and then check
+                            your knowledge with <strong> Tests</strong>. <br />
+                            If there is anything more you would like to ask about go to
+                            our <strong>Forum</strong>.
+                        </p>
+                    </div>
                 </div>
-                <div className={`${styles.slideWrapper}`}>
-                    <div className={styles.slide}>Forum description</div>
+                <div className={styles.slideWrapper}>
+                    <div className={styles.slide}>
+                        <img
+                            className={styles.descriptionImage}
+                            src={ForumImage}
+                            alt="forumImage"
+                        />
+                        <h2 className={styles.slideHeader}>Forum!</h2>
+                        <p className={styles.slideText}>
+                            Forum description. <br />
+                            You can explore <strong>Flashcards</strong> and then check
+                            your knowledge with <strong> Tests</strong>. <br />
+                            If there is anything more you would like to ask about go to
+                            our <strong>Forum</strong>.
+                        </p>
+                    </div>
                 </div>
             </Slider>
         </div>
